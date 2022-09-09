31e26a620e3fe6e5a7cbdc866afa55e63f8bf0ad
Nawaz Danish <nawazdanish1996@gmail.com>
2022-09-10 01:31:31 +0530

    initial commit

--- DSA 1 & 2/4 Loops/LoopsInJava.java
+++ DSA 1 & 2/4 Loops/LoopsInJava.java
@@ -1,12 +1,29 @@
 //package
 import java.util.*;
-
 class LoopsInJava {
     public static void main(String args[]){
         //input
         //System.in is a standard input stream
         Scanner sc = new Scanner(System.in);
-        // code ....
+        // for Loop
+
+        // Print Hello World five time
+        // for(int i = 0; i < 5 ; i++){
+            // System.out.println("Hello World");
+        // }
+
+        // print 1-10
+        // i++ means i + 1
+        for(int counter = 0; counter < 11; counter++){
+            // System.out.println(counter);
+            // System.out.print(counter+ " ");
+        }
+
+
+        for(int i = 0; i < 11; i++){
+            System.out.print(i+ " ");
+        }
+
         sc.close(); // 'sc' is never closed problem solved 
     }
     
---
+++ DSA 1 & 2/4 Loops/Quiz.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Quiz {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        // Print 1+2+3+4=10
+        // int n = sc.nextInt();
+        // int sum = 0;
+
+        // for(int i=0; i<=n; i++){
+        //     sum = sum + i;
+        //     System.out.println(sum);
+        // }
+
+        // print 2*2 to 20
+        for(int i=1; i<=10; i++){
+            System.out.println(i*2);
+        }
+
+
+        sc.close();
+    }
+}
