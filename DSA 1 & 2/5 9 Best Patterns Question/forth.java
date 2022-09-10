b61f60a0688a9c40ae6ef040e1cd15d7479e80b9
Nawaz Danish <nawazdanish1996@gmail.com>
2022-09-11 01:06:36 +0530

    initiala commit

--- DSA 1 & 2/5 9 Best Patterns Question/forth.java
+++ DSA 1 & 2/5 9 Best Patterns Question/forth.java
@@ -1,12 +1,10 @@
-public class first {
+public class forth {
     public static void main(String args[]){
-        int n = 4;
-        int m = 5;
 
-        // outer loop
-        for(int i=0; i<n; i++){
-            //inner loop
-            for(int j = 0; j<m; j++){
+        int n = 4;
+
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
                 System.out.print("*");
             }
             System.out.println();
