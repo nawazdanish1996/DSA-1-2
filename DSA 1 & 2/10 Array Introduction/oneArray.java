36a2ea776fe55a8542c895e17ec8e3b7807a8ca8
Nawaz Danish <nawazdanish1996@gmail.com>
2022-09-12 02:14:20 +0530

    add

---
+++ DSA 1 & 2/10 Array Introduction/oneArray.java
@@ -0,0 +1,19 @@
+public class oneArray {
+    public static void main(String args[]){
+
+        // int[] marks = new int[3];
+
+        int marks[] = {97, 95, 55};
+        // marks[0] = 97; //Physics
+        // marks[1] = 95; // Chemistry
+        // marks[2] = 55; // English
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
