0550eead11a3babdba8a57bd7b8f5e7cd07122ba
Nawaz Danish <nawazdanish1996@gmail.com>
2022-09-14 04:10:14 +0530

    updated

---
+++ DSA with codeitup (Array & String)/Array Lesson/ArrayIntro.java
@@ -0,0 +1,17 @@
+/*
+ * What is Array?
+ * 
+ * Array is a collection of similar types of data,
+ * that means we want to store multiple value of similar types,
+ * than we use concept of Array.
+ */
+
+ /*
+  * int arr[] = {20, 25, 50, 56};
+  */
+
+ /*
+  * Syntex
+
+  int arr[] = new int[5];
+  */
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://www.youtube.com/playlist?list=PLfqMhTWNBTe3LtFWcvwpqTkUSlB32kJop
 
 Codeitup:
 https://www.youtube.com/playlist?list=PLmRclvVt5DtnqhXTJwd-oqVRwO3bLZCGV
+
+java interview questions:
+https://www.youtube.com/playlist?list=PLqReTILjnqIUkTuOpLWj3kBnnH-119hlH
