deb51b453f61c2d083f9954c6cb6e604e3bc19b9
Nawaz Danish <nawazdanish1996@gmail.com>
2022-10-14 15:09:45 +0530

    updated

---
+++ DSA with Geeky Shows/020/first.java
@@ -0,0 +1,10 @@
+public class first{
+
+    public static void main(String args[]){
+
+        int number = 10;
+        float num = 10.3f;
+        double n = 10.588;
+        char word = 'a';
+    }
+}
--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Nawaz Danish
 
 
 DSA with Geeky shows
-
 Playlist: https://www.youtube.com/playlist?list=PLbGui_ZYuhij8Oplrvjt_RlDliZQgdxoV
+
+
+20-326
