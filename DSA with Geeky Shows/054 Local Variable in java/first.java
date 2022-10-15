fb0ef3933ad1ffcfab4fc62eaa92cc027b902338
Nawaz Danish <nawazdanish1996@gmail.com>
2022-10-15 16:51:33 +0530

    initial commit

---
+++ DSA with Geeky Shows/054 Local Variable in java/first.java
@@ -0,0 +1,9 @@
+import java.rmi.server.RemoteCall;
+
+class firstClass{
+
+    public static void main(String args[]){
+        int Roll = 10;
+        System.out.println("Roll No. "+ Roll);
+    }
+}
---
+++ DSA with Geeky Shows/060 Method return string in java/first.java
@@ -0,0 +1,7 @@
+class firstClass{
+
+    public static void main(String args[]){
+        String name = "Nawaz Danish";
+        System.out.println(name);
+    }
+}
