8d5b97c6168edd9a3205b9aac301004a3a3e5aa6
Nawaz Danish <nawazdanish1996@gmail.com>
2022-11-14 22:29:11 +0530

    updated

---
+++ DSA with Apna College/src/com/linkedlist/LL.java
@@ -0,0 +1,19 @@
+package com.linkedlist;
+
+public class LL {
+    class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // add - first,last
+
+
+    public static void main(String[] args) {
+        LL list = new LL();
+    }
+}
---
+++ DSA with Apna College/src/com/nawaz/Main.java
@@ -0,0 +1,8 @@
+package com.nawaz;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
